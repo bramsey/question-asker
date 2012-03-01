85f5f222b6a0e775cf46aa44bc94087b1a9240a9
Bill Ramsey <bill@vybly.com>
2012-03-01 02:17:10 -0500

    fixed bug with undo and cached jquery references

--- coffee/questions.coffee
+++ coffee/questions.coffee
@@ -5,6 +5,14 @@ YES_KEY = 89
 NO_KEY = 78
 UNDO_KEY = 85
 item_index = 0
+question_div = $('#question')
+answer_div = $('#answer')
+item = $('#item')
+undo = $('#undo')
+yes_area = $('#yes_area')
+no_area = $('#no_area')
+question_input = $('#question_input')
+question_action = $('#question_action')
 
 $('#list_action').live 'click', (event) ->
 	# populate the list array and remove leading and trailing empty results
@@ -12,27 +20,27 @@ $('#list_action').live 'click', (event) ->
 	
 	# transition to the question stage
 	$('#list').hide()
-	$('#question').fadeIn('fast')
+	question_div.fadeIn('fast')
 	
-$('#question_action').live 'click', (event) ->
+question_action.live 'click', (event) ->
 	# assign the question
-	question = $('#question_input').attr('value')
+	question = question_input.attr('value')
 	$('.question').html(question)
 	
 	# transition to the answering stage
-	$('#item').html(list[item_index])
-	$('#question').hide()
-	$('#answer').fadeIn('fast')
+	item.html(list[item_index])
+	question_div.hide()
+	answer_div.fadeIn('fast')
 
 $('.answer').live 'click', (event) ->
 	check_action(this)
 	
 	# show undo button if not on the first item
-	$('#undo').fadeIn('fast') if item_index >= 0
+	undo.fadeIn('fast') if item_index >= 0
 	
 	# display the results when done
 	if (item_index >= list.length - 1) # done
-		$('#answer').hide()
+		answer_div.hide()
 		$('#results').fadeIn('fast')
 	else
 		item_index += 1
@@ -42,43 +50,43 @@ $('.answer').live 'click', (event) ->
 			list.splice(item_index, 1)
 			
 		# show the next item
-		$('#item').html(list[item_index])
+		item.html(list[item_index])
 
 # store votes based on action clicked and add to right text area
 check_action = (action) ->
 	if action.id is 'yes_action'
 		list_votes[item_index] = 'yes'
-		$('#yes_area').append(list[item_index] + '\n')
+		yes_area.append(list[item_index] + '\n')
 	else if action.id is 'no_action'
 		list_votes[item_index] = 'no'
-		$('#no_area').append(list[item_index] + '\n')
+		no_area.append(list[item_index] + '\n')
 	else
 		# resets to the previous item for re-answering
-		$('#undo').hide() if item_index == 1
+		undo.hide() if item_index == 1
 		item_index -= 1
 		if list_votes[item_index] is 'yes'
-			remove_last_item('#yes_area')
+			remove_last_item(yes_area)
 		else
-			remove_last_item('#no_area')
+			remove_last_item(no_area)
 		item_index -= 1
 
 # removes the last item from the specified text area
 remove_last_item = (area) ->
-	val = $(area).attr('value').split('\n')
+	val = area.attr('value').replace( /^\s+|\s+$/g, "").split('\n')
 	val.splice(val.length-1, 1)
-	$(area).html(val.join())
+	area.html(val.join('\n'))
 
 	
 # shortcut listener
 $(document).live 'keyup', (event) ->
-	if($('#answer').css('display') != 'none')
+	if(answer_div.css('display') != 'none')
 		switch event.keyCode
 			when YES_KEY then $('#yes_action').trigger('click')
 			when NO_KEY then $('#no_action').trigger('click')
-			when UNDO_KEY then $('#undo').trigger('click') if $('#undo').css('display') != 'none'
+			when UNDO_KEY then undo.trigger('click') if undo.css('display') != 'none'
 
 # allows using the enter key to submit the question			
-$('#question_input').live 'keydown', (event) ->
+question_input.live 'keydown', (event) ->
 	if event.keyCode is 13
-		$('#question_action').trigger('click')
+		question_action.trigger('click')
 		
--- js/questions.js
+++ js/questions.js
@@ -1,5 +1,5 @@
 (function() {
-  var NO_KEY, UNDO_KEY, YES_KEY, check_action, item_index, list, list_votes, question, remove_last_item;
+  var NO_KEY, UNDO_KEY, YES_KEY, answer_div, check_action, item, item_index, list, list_votes, no_area, question, question_action, question_div, question_input, remove_last_item, undo, yes_area;
 
   list = [];
 
@@ -15,49 +15,65 @@
 
   item_index = 0;
 
+  question_div = $('#question');
+
+  answer_div = $('#answer');
+
+  item = $('#item');
+
+  undo = $('#undo');
+
+  yes_area = $('#yes_area');
+
+  no_area = $('#no_area');
+
+  question_input = $('#question_input');
+
+  question_action = $('#question_action');
+
   $('#list_action').live('click', function(event) {
     list = $('#list_area').attr('value').replace(/^\s+|\s+$/g, "").split('\n');
     $('#list').hide();
-    return $('#question').fadeIn('fast');
+    return question_div.fadeIn('fast');
   });
 
-  $('#question_action').live('click', function(event) {
-    question = $('#question_input').attr('value');
+  question_action.live('click', function(event) {
+    question = question_input.attr('value');
     $('.question').html(question);
-    $('#item').html(list[item_index]);
-    $('#question').hide();
-    return $('#answer').fadeIn('fast');
+    item.html(list[item_index]);
+    question_div.hide();
+    return answer_div.fadeIn('fast');
   });
 
   $('.answer').live('click', function(event) {
     check_action(this);
-    if (item_index >= 0) $('#undo').fadeIn('fast');
+    if (item_index >= 0) undo.fadeIn('fast');
     if (item_index >= list.length - 1) {
-      $('#answer').hide();
+      answer_div.hide();
       return $('#results').fadeIn('fast');
     } else {
       item_index += 1;
       while (list[item_index].replace(/^\s+|\s+$/g, "") === '') {
         list.splice(item_index, 1);
       }
-      return $('#item').html(list[item_index]);
+      return item.html(list[item_index]);
     }
   });
 
   check_action = function(action) {
     if (action.id === 'yes_action') {
       list_votes[item_index] = 'yes';
-      return $('#yes_area').append(list[item_index] + '\n');
+      return yes_area.append(list[item_index] + '\n');
     } else if (action.id === 'no_action') {
       list_votes[item_index] = 'no';
-      return $('#no_area').append(list[item_index] + '\n');
+      return no_area.append(list[item_index] + '\n');
     } else {
-      if (item_index === 1) $('#undo').hide();
+      if (item_index === 1) undo.hide();
       item_index -= 1;
       if (list_votes[item_index] === 'yes') {
-        remove_last_item('#yes_area');
+        remove_last_item(yes_area);
       } else {
-        remove_last_item('#no_area');
+        remove_last_item(no_area);
       }
       return item_index -= 1;
     }
@@ -65,28 +81,26 @@
 
   remove_last_item = function(area) {
     var val;
-    val = $(area).attr('value').split('\n');
+    val = area.attr('value').replace(/^\s+|\s+$/g, "").split('\n');
     val.splice(val.length - 1, 1);
-    return $(area).html(val.join());
+    return area.html(val.join('\n'));
   };
 
   $(document).live('keyup', function(event) {
-    if ($('#answer').css('display') !== 'none') {
+    if (answer_div.css('display') !== 'none') {
       switch (event.keyCode) {
         case YES_KEY:
           return $('#yes_action').trigger('click');
         case NO_KEY:
           return $('#no_action').trigger('click');
         case UNDO_KEY:
-          if ($('#undo').css('display') !== 'none') {
-            return $('#undo').trigger('click');
-          }
+          if (undo.css('display') !== 'none') return undo.trigger('click');
       }
     }
   });
 
-  $('#question_input').live('keydown', function(event) {
-    if (event.keyCode === 13) return $('#question_action').trigger('click');
+  question_input.live('keydown', function(event) {
+    if (event.keyCode === 13) return question_action.trigger('click');
   });
 
 }).call(this);
